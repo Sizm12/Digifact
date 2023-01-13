918c5152ce76de9346deae68993fa6dfa0f8488c
Sizm12 <s.zeledon.cbe@gmail.com>
2023-01-13 07:10:47 +0000

    Commit13

--- bills_digifact/models/digifact.py
+++ bills_digifact/models/digifact.py
@@ -1,7 +1,12 @@
 from odoo import models, fields, api
+from odoo.exceptions import UserError
+import request
 
 class account_move_inherit(models.Model):
     _inherit='account.move'
     
     def test_button_function(self):
-        raise Warning('Prueba de Boton')
+        URL= "https://felgttestaws.digifact.com.gt/gt.com.fel.api.v3/api/login/get_token"
+        Params={"Username":"GT.000041545036.TESTUSER","Password":"j6C7&f5?"}
+        response= request.post(url=URL, data=Params)
+        raise UserError(_('Peticion a Digifact is %s'%response.text))
