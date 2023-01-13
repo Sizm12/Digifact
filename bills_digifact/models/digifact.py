16d364008912aa622341555313f3445e59f8e85b
Sizm12 <s.zeledon.cbe@gmail.com>
2023-01-13 06:33:15 +0000

    Commit12

--- bills_digifact/models/digifact.py
+++ bills_digifact/models/digifact.py
@@ -4,4 +4,4 @@ class account_move_inherit(models.Model):
     _inherit='account.move'
     
     def test_button_function(self):
-        print('Prueba de Boton')
+        raise Warning('Prueba de Boton')
