1552a867bf966ce275f69fbca545c3605459d8db
Sizm12 <s.zeledon.cbe@gmail.com>
2023-01-13 07:49:04 +0000

    Commit 123

--- bills_digifact/models/digifact.py
+++ bills_digifact/models/digifact.py
@@ -1,6 +1,6 @@
 from odoo import models, fields, api
 from odoo.exceptions import UserError
-from requests import requests
+import requests
 
 class account_move_inherit(models.Model):
     _inherit='account.move'
