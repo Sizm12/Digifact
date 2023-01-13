51b4b81e2da3b95d12f519c4dae870f7ca350220
Sizm12 <s.zeledon.cbe@gmail.com>
2023-01-13 05:42:47 +0000

    Commit3

--- bills_digifact/models/.ipynb_checkpoints/digifact-checkpoint.py
+++ bills_digifact/models/.ipynb_checkpoints/digifact-checkpoint.py
@@ -1,6 +1,6 @@
 from odoo import models, fields, api
 
-class Invoice Digifact(models.Model):
+class invoice_digifact(models.Model):
     _inherit='account.move'
     
     def test_button_function(self):
