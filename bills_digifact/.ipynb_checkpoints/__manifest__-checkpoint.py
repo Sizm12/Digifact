3c2e5afe711ae515ff245e742ecf0d9e8b6db501
Sizm12 <s.zeledon.cbe@gmail.com>
2023-01-13 06:11:18 +0000

    Commit12

--- bills_digifact/.ipynb_checkpoints/__manifest__-checkpoint.py
+++ bills_digifact/.ipynb_checkpoints/__manifest__-checkpoint.py
@@ -20,7 +20,7 @@
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['base', 'account.move'],
+    'depends': ['account'],
 
     # always loaded
     'data': [
