1d1c52606594c26e6b86c3b02d47092119bed1c3
Sizm12 <s.zeledon.cbe@gmail.com>
2023-01-13 07:31:37 +0000

    Commit 123

--- bills_digifact/.ipynb_checkpoints/__manifest__-checkpoint.py
+++ bills_digifact/.ipynb_checkpoints/__manifest__-checkpoint.py
@@ -17,7 +17,7 @@
     # Check https://github.com/odoo/odoo/blob/14.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
     'category': 'Uncategorized',
-    'version': '0.1',
+    'version': '0.2',
 
     # any module necessary for this one to work correctly
     'depends': ['account'],
@@ -33,4 +33,8 @@
     'demo': [
         'demo/demo.xml',
     ],
+    
+    'external_dependencies': {
+        'python': ['request'],
+    },
 }
