fff5ccf689168c390ebe37c6ac2bd080b2bf43aa
Sizm12 <s.zeledon.cbe@gmail.com>
2023-01-18 07:34:11 +0000

    Test get data of field

--- bills_digifact/models/.ipynb_checkpoints/digifact-checkpoint.py
+++ bills_digifact/models/.ipynb_checkpoints/digifact-checkpoint.py
@@ -10,9 +10,9 @@ class account_move_inherit(models.Model):
         for rec in self:
             customer = rec.partner_id.name
             
-            for item in rec.invoice_line_ids
-            var= item.product_id.name
-            raise UserError(_('El cliente es %s'%var)) 
+            for item in rec.invoice_line_ids:
+                var= item.product_id.name
+                raise UserError(_('El cliente es %s'%var)) 
 
         #URL= "https://felgttestaws.digifact.com.gt/gt.com.fel.api.v3/api/login/get_token"
         #Params={"Username":"GT.000041545036.TESTUSER","Password":"j6C7&f5?"}
