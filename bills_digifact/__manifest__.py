b97b664e02bb594400081859bf4bb60bff33557b
Sizm12 <s.zeledon.cbe@gmail.com>
2023-01-19 03:57:20 +0000

    Change

--- bills_digifact/__manifest__.py
+++ bills_digifact/__manifest__.py
@@ -17,7 +17,7 @@
     # Check https://github.com/odoo/odoo/blob/14.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
     'category': 'Uncategorized',
-    'version': '0.2',
+    'version': '0.3',
 
     # any module necessary for this one to work correctly
     'depends': ['account'],
@@ -36,5 +36,6 @@
     
     'external_dependencies': {
         'python': ['requests'],
+        'python': ['xml.etree.ElementTree'],
     },
 }
